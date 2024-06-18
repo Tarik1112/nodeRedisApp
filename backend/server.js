d5e50c602a0786487b2d292f7a23bbfb2fe251a1
Tarik1112 <tare.mujke@gmail.com>
2024-06-18 10:57:15 +0200

    Frontend removed

--- backend/server.js
+++ backend/server.js
@@ -1,91 +1,130 @@
 const express = require('express');
 const redis = require('redis');
 const cors = require('cors');
+const util = require('util');
+const axios = require('axios');
 
 const app = express();
-const client = redis.createClient();
 
-app.use(cors);
-app.use((req, res ,next) =>
-{
-    res.header("Access-Control-Allow-Origin","*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-        );
-    if (req.method === 'OPTIONS')
-    {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
+const client = redis.createClient({
+  url: "redis://127.0.0.1:6379",
+  legacyMode: true
+})
 
-  // Pass next middleware
-  next();
+client.set = util.promisify(client.set);
+client.get = util.promisify(client.get);
 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get('/test', (req, res) => {
-  console.log('Received a GET HTTP method');
-  res.send("Teeetetete")
-});
+
 
 (async () => {
-  // Connect to redis server
   await client.connect();
 })();
 
-// async function nodeRedisDemo() {
-//     try {
-//       await client.connect();
-  
-//       await client.set('name', 'Hello from node redis');
-//       const myKeyValue = await client.get('name');
-//       console.log(myKeyValue);
-  
-//       const numAdded = await client.zAdd('vehicles', [
-//         {
-//           score: 4,
-//           value: 'car',
-//         },
-//         {
-//           score: 2,
-//           value: 'bike',
-//         },
-//       ]);
-//       console.log(`Added ${numAdded} items.`);
-  
-//       for await (const { score, value } of client.zScanIterator('vehicles')) {
-//         console.log(`${value} -> ${score}`);
-//       }
-  
-//       await client.quit();
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   }
-  
-//   nodeRedisDemo();
- 
-
 client.on('connect', () => {
-    console.log('Connected to Redis!');
+  console.log("connected redis");
+})
+
+app.get('/users', (req, res) => {
+  console.log('Received GET request for /users');
+  client.zrange('users', 0, -1, (err, users) => {
+    if (err) {
+      console.error('Error fetching users from Redis:', err);
+      res.status(500).send(err);
+    } else {
+      console.log('Users fetched from Redis:', users);
+      res.send(users);
+    }
+  });
+});
+
+app.post("/" , async (req, res) => {
+  const {key, value} = req.body;
+  const response = await client.set(key,value);
+
+
+  res.send(response);
 })
 
 
-
-app.get('/users', (req, res) => {
-    console.log('Received GET request for /users');
-    client.zrange('users', 0, -1, (err, users) => {
-      if (err) {
-        console.error('Error fetching users from Redis:', err);
-        res.status(500).send(err);
-      } else {
-        console.log('Users fetched from Redis:', users);
-        return res.send(users);
-      }
-    });
+app.post('/user', async (req, res) => {
+  const { id, username, age } = req.body;
+  console.log('Received POST request to add user:', req.body);
+  
+  // Ensure id, username, and age are strings
+  if (!id || !username || !age) {
+    return res.status(400).send('Invalid input');
+  }
+  
+  await client.hSet(`user:${id}`, 'username', username, 'age', age.toString(), (err, reply) => {
+    if (err) {
+      console.error('Error adding user to Redis:', err);
+      res.status(500).send(err);
+    } else {
+      console.log('User added to Redis:', reply);
+      client.zadd('users', 0, `user:${id}`, (zerr, zreply) => {
+        if (zerr) {
+          console.error('Error adding user to sorted set:', zerr);
+          res.status(500).send(zerr);
+        } else {
+          console.log('User added to sorted set:', zreply);
+          res.send('User added');
+        }
+      });
+    }
   });
+});
+
+const fetchPostsFromAPI = async () => {
+
+  console.log("Fetched")
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data from API:', error);
+    throw error;
+  }
+};
+
+// const cacheMiddleware = (req, res, next) => {
+//   const { key } = req.params;
+//   client.get(key, (err, data) => {
+//     if (err) throw err;
+//     if (data) {
+//       res.send(JSON.parse(data));
+//     } else {
+//       next();
+//     }
+//   });
+// };
+
+app.get('/posts', async (req, res) => {
+  try {
+    const cachedPosts = await client.get('posts');
+
+    if(cachedPosts){
+      console.log("kesha")
+      return res.json(JSON.parse(cachedPosts))
+    }
+    console.log("nekesa")
+    const posts = await fetchPostsFromAPI();
+
+    client.setEx('posts', 3600, JSON.stringify(posts)); // Caching for 1 hour
+    
+    res.send(posts);
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server is running on port 3000');
 });
