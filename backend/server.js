5a4ab6477b7e38ab5ec4edbdf40e23913a123601
Tarik1112 <tare.mujke@gmail.com>
2024-06-19 16:04:53 +0200

    Backend

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,8 @@ client.on('connect', () => {
   console.log("connected redis");
 })
 
+//Caching local data
+
 app.get('/users', (req, res) => {
   console.log('Received GET request for /users');
   client.zrange('users', 0, -1, (err, users) => {
@@ -43,14 +45,6 @@ app.get('/users', (req, res) => {
     }
   });
 });
-
-app.post("/" , async (req, res) => {
-  const {key, value} = req.body;
-  const response = await client.set(key,value);
-
-
-  res.send(response);
-})
 
 
 app.post('/user', async (req, res) => {
@@ -81,6 +75,8 @@ app.post('/user', async (req, res) => {
   });
 });
 
+//Caching external data with API call
+
 const fetchPostsFromAPI = async () => {
 
   console.log("Fetched")
@@ -93,37 +89,64 @@ const fetchPostsFromAPI = async () => {
   }
 };
 
-// const cacheMiddleware = (req, res, next) => {
-//   const { key } = req.params;
-//   client.get(key, (err, data) => {
-//     if (err) throw err;
-//     if (data) {
-//       res.send(JSON.parse(data));
-//     } else {
-//       next();
-//     }
-//   });
-// };
 
 app.get('/posts', async (req, res) => {
   try {
     const cachedPosts = await client.get('posts');
 
     if(cachedPosts){
-      console.log("kesha")
       return res.json(JSON.parse(cachedPosts))
     }
-    console.log("nekesa")
-    const posts = await fetchPostsFromAPI();
-
+    const posts = await fetchPostsFromAPI()
     client.setEx('posts', 3600, JSON.stringify(posts)); // Caching for 1 hour
-    
     res.send(posts);
 
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
+
+//Message Queues
+
+app.post('/sendToQueue', async (req, res) =>{
+    const task = req.body.task;
+    await client.lPush('tasks', JSON.stringify(task));
+    res.send('Task added to queue')
+})
+
+app.get('/getQueueData', async (req, res) => {
+  console.log('Received GET request for /getQueueData');
+
+  try{
+
+    await client.lRange('tasks', 0, -1, (err, tasks) => {
+      console.log('tasks', tasks);
+      let entries = [];
+      if(tasks){
+
+        tasks.forEach(task => {
+          
+          const parsedTask = JSON.parse(task);
+          entries.push(parsedTask);
+
+        })
+        
+        console.log("Parsed entries: ", entries);
+        res.send(entries);
+        client.lTrim('tasks', tasks.length, -1);
+      }else{
+        console.log("No tasks to process...")
+      }
+    })
+
+    
+    
+  }catch(error){
+    console.log("Error processing tasks: ", error);
+  }
+
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
